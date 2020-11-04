b5d78a7183b439352c43f9895868ffcb369d0a3f
Jeongmin Cho <qll4359@naver.com>
2020-11-04 21:11:13 +0900

    DOM Handliing

--- Practice/Javascript/DOM 핸들링/index.js
+++ Practice/Javascript/DOM 핸들링/index.js
@@ -5,4 +5,4 @@ document.body.append(text4);
 
 console.log(document.getElementById('t1').innerText);
 console.log(document.querySelectorAll('div')[1].innerText);
-console.log(document.querySelector('.t3').innerText);
+console.log(document.querySelector('.t3').innerText); 
