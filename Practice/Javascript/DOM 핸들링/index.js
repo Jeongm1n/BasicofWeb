6165398eb68993fa901537607f9017e00bdbc4b3
Jeongmin Cho <qll4359@naver.com>
2020-11-04 21:08:09 +0900

    Add files via upload

---
+++ Practice/Javascript/DOM 핸들링/index.js
@@ -0,0 +1,8 @@
+let text4 = document.createElement('h2');
+text4.innerText='Text4';
+
+document.body.append(text4);
+
+console.log(document.getElementById('t1').innerText);
+console.log(document.querySelectorAll('div')[1].innerText);
+console.log(document.querySelector('.t3').innerText);
